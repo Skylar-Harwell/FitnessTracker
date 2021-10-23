89baf8590aeb594670e8b3f160367714d2d054a1
Skylar Harwell <swharwell@gmail.com>
2021-10-23 09:51:08 -0400

    Initial Commit including file structure

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+  
+
+module.exports = db;
