3f73d4c19217ddf409e00fca5502ca7c4feff813
Skylar Harwell <swharwell@gmail.com>
2021-10-23 14:52:25 -0400

    Updated Server for Mongoose

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongojs = require("mongojs");
-const logger = require("morgan");
+const mongoose = require("mongoose");
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,7 +9,7 @@ const db = require("./models");
 
 const app = express();
 
-app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
